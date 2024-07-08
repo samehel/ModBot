55a05029939cee74428cf5ddd4f3ec63a898fd08
Sameh <sameh.elwakeel75@gmail.com>
2024-07-08 19:17:27 +0300

    fixed spelling mistake

--- Database/Connection.js
+++ Database/Connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try {
-        const conn = await mongoose.connect(process.env.MANGODB_URI);
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (e) {
         console.error(e);
